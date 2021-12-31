ee2e45c5abbd8bf51719b7c8ab60ab0ffda9a11d
Tariq Hook <thook@Tariqs-MacBook-Pro-2.local>
2021-12-31 11:52:29 -0500

    changed some stuff

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Wutang Forever
         </a>
       </header>
     </div>
